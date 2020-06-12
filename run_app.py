5ecaf5881956adf81299f1a058cf787d928283bd
Jhanvi Arora <63912099+jhanvi0905@users.noreply.github.com>
2020-06-12 22:32:29 +0530

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -9,6 +9,6 @@ nlu_interpreter = RasaNLUInterpreter('./models/chatbotnlu/default/chatbotnlu')
 action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
 agent = Agent.load('./models/current/dialogue', interpreter = nlu_interpreter, action_endpoint = action_endpoint)
 
-input_channel = SlackInput('xoxb-648575328130-655090052769-QhOthMP9CvFMxvthqyDl5eJv')
+input_channel = SlackInput('Slack Token')
 
 agent.handle_channels([input_channel], 5004, serve_forever=True)
